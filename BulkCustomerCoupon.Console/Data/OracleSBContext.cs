c921cd843e145e61ffffab54a92a08ffe7984094
Akhilesh Mishra <akhileshmishra.developer@gmail.com>
2023-07-14 10:42:08 +0545

    added: oracle-database-connection

---
+++ BulkCustomerCoupon.Console/Data/OracleSBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkCustomerCoupon.Console.Data
+{
+    public class OracleDBContext
+        : DbContext
+    {
+        public OracleDBContext(DbContextOptions<OracleDBContext> options)
+            : base(options)
+        {
+            
+        }
+        
+
+    }
+}
--- BulkCustomerCoupon.Console/Program.cs
+++ BulkCustomerCoupon.Console/Program.cs
@@ -2,13 +2,18 @@
 using BulkCustomerCoupon.Console.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Oracle.ManagedDataAccess.Client;
 
 var testConnectionString = "Data Source=localhost;initial catalog=Coupon;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+var oracleConnectionString = "Provider=OraOLEDB.Oracle;Data Source=192.168.20.123:1521;User Id=hikmat;Password=hikmat;";
 var serviceProvider = new ServiceCollection()
             .AddDbContext<SqlServerDbContext>(opts => opts.UseSqlServer(testConnectionString))
+            .AddDbContext<OracleDBContext>(opts => opts.UseOracle(oracleConnectionString))
             .BuildServiceProvider();
 var sqlServerDbContext = serviceProvider.GetService<SqlServerDbContext>();
+var oracleDbContext = serviceProvider.GetService<OracleDBContext>();
 
+var isConnected = oracleDbContext.Database.CanConnect();
 var ppvCustomers = await sqlServerDbContext.PPVCustomers.Where(x => x.IsCouponCreated == null).ToListAsync();
 
 for(int i = 0; i < ppvCustomers.Count; i++)
